dabc0ffba67edffff90a28f2dc59ac4a6f8f8f6e
yusufajiib <51849143+yusufajiib@users.noreply.github.com>
2019-06-27 15:07:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -119,7 +119,7 @@ if (isset($_POST['submit'])) {
 		<h3>2. Kemudian copikan salah satu halaman web gambar diatas di bawah sini !</h3>
 		<script type="text/javascript">
 		
-    		function myFunction(myInput) {
+    		function myFunction(copyText, tooltip) {
       			var copyText = document.getElementById("myInput");
       			copyText.select();
       			document.execCommand("copy");
